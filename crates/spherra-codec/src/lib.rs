a2a7d297104b70eedbf683150f7c2a8ebbde6b78
bhrugusetlur-art <bhrugu.setlur@gmail.com>
2026-08-04 19:37:55 -0700

    feat(codec): add direct int4 tiled scalar layout

--- crates/spherra-codec/src/lib.rs
+++ crates/spherra-codec/src/lib.rs
@@ -1,7 +1,11 @@
 #![deny(unsafe_code)]
 
+pub mod int4;
 mod spec;
+mod tiled_soa;
 mod transform;
 
+pub use int4::{DirectCode, DirectCodeError, QuantizerTable, RadiusFlags};
 pub use spec::TransformSpec;
+pub use tiled_soa::TiledSoa32;
 pub use transform::{TransformPlan, TransformedDirection, inverse_transform, transform};
